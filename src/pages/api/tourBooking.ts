8dbaaff60032e2642f876b28a41d4154799f7455
Mwero Abdalla <mwerothewebmaker@gmai.com>
2023-03-08 04:34:02 +0300

    fix: bug-fix in the orders

--- src/pages/api/tourBooking.ts
+++ src/pages/api/tourBooking.ts
@@ -23,27 +23,26 @@ export default async function handler(
       // const {error, flag} = await sendConfirmationEmail(JSON.parse(req.body).email,JSON.parse(req.body).fullName);
       res.status(200).json({ createdBooking, data });
       return
-      // }
-      let msg="There was an issue creating the user"
-      res.status(200).json({...resp, flag:true,error:msg , createdBooking});
-      return;
+    //   // }
+    //   let msg="There was an issue creating the user"
+    //   res.status(200).json({...resp, flag:true,error:msg , createdBooking});
+    //   return;
       
-    try {
-      const createdBooking = await prisma.tourBooking.create({data})
-      // console.log({createdBooking})
-      if(createdBooking){
-      // const {error, flag} = await sendConfirmationEmail(JSON.parse(req.body).email,JSON.parse(req.body).fullName);
-      res.status(200).json({...resp, flag:true, createdBooking });
-      return
-      }
-      let msg="There was an issue creating the user"
-      res.status(200).json({...resp, flag:true,error:msg , createdBooking});
-      // res.status(200).json({...resp})
+    // try {
+    //   const createdBooking = await prisma.tourBooking.create({data})
+    //   // console.log({createdBooking})
+    //   if(createdBooking){
+    //   // const {error, flag} = await sendConfirmationEmail(JSON.parse(req.body).email,JSON.parse(req.body).fullName);
+    //   res.status(200).json({...resp, flag:true, createdBooking });
+    //   return
+    //   }
+    //   let msg="There was an issue creating the user"
+    //   res.status(200).json({...resp, flag:true,error:msg , createdBooking});
+    //   // res.status(200).json({...resp})
       
-    } catch (error) {
-      res.status(201).json({msg:"There was an issue creating the user"})
-    }
+    // } catch (error) {
+    //   res.status(201).json({msg:"There was an issue creating the user"})
+    // }
  
-  } else {
-  }
+  } 
 }
