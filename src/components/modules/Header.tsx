6669205f669c790ecce6590f4a4ab6925cc9dbf5
Mwero Abdalla <mwerothewebmaker@gmai.com>
2023-03-19 06:40:18 +0300

    task: add a mobile menubar

--- src/components/modules/Header.tsx
+++ src/components/modules/Header.tsx
@@ -1,14 +1,38 @@
 
-import { FaSearch } from 'react-icons/fa';
+import { useState } from 'react';
+import { FaBars, FaSearch, FaTimes } from 'react-icons/fa';
+
 import Navbar from './Navbar';
 
 
 export default function Header() {
+  const [navToggle, setNavToggle] = useState<boolean>(false);
   return (
-    <header className="bg-gray-800 p-3 w-full rounded-t-md">
-      <div className='flex flex-col items-center  sm:flex-row '>
+    <header className="w-full pt-10  sm:pt-0 fixed top-0 right-0">
+      <div className='h-[50px] sm:hidden  relative'>
+        {/* <div></div> */}
+      <button
+        onClick={() => setNavToggle(!navToggle)}
+        className="cursor-pointer pr-4 z-10 sm:hidden  absolute top-1  right-0 "
+      >
+        {navToggle ? <FaTimes size={30} className='text-white'/> : <FaBars size={30} />}
+      </button>
+      </div>
+     
+      <div className={`${navToggle ? 'flex' : 'hidden'} flex-col items-center  sm:flex-row  bg-gray-800 p-3 w-full rounded-t-md`}>
+
         <Navbar />
-        <div className="relative ml-auto ">
+        <div className="w-full md:w-1/3 flex-1 max-w-fit relative ml-auto ">
+          <input type="text" placeholder="Search..." className="bg-gray-700 p-2 text-white rounded-full w-full flex items-center justify-center" />
+          <button className="absolute right-0 top-0 mr-3 mt-1 p-2">
+            <FaSearch className="fill-current text-white " />
+          </button>
+        </div>
+      </div>
+      <div className={`hidden sm:flex flex-col items-center  sm:flex-row  bg-gray-800 p-3 w-full rounded-t-md`}>
+
+        <Navbar />
+        <div className="w-full md:w-1/3 flex-1 max-w-fit relative ml-auto ">
           <input type="text" placeholder="Search..." className="bg-gray-700 p-2 text-white rounded-full w-full flex items-center justify-center" />
           <button className="absolute right-0 top-0 mr-3 mt-1 p-2">
             <FaSearch className="fill-current text-white " />
--- src/components/modules/Navbar.tsx
+++ src/components/modules/Navbar.tsx
@@ -1,19 +1,24 @@
 import Link from 'next/link';
 import { navLinks } from '../../constants';
 
-export default function Navbar ()  {
+const mainNav = (link: string, label: string) => <Link href={link} className="w-full md:inherit bg-gray-700 sm:bg-inherit my-2 text-white text-center font-medium block py-2 px-4">{label}</Link>
+const navContent = navLinks.map(({ link, label }: { link: string, label: string, icon?: string }) => <li key={link} className="w-full md:flex-1">
+  {mainNav(link, label)}
+</li>)
+
+export default function Navbar() {
   return (
-    <nav className=" w-auto">
-      
-        <ul className="w-full flex flex-col sm:flex-row items-center ">
-          {
-            navLinks.map(({link,label}:{link:string,label:string,icon?:string})=><li key ={link}>
-            <Link href={link} className="w-full md:inherit bg-gray-700 sm:bg-inherit my-2 text-white text-center font-medium block py-2 px-4">{label}</Link>
-          </li>)}
-          
-         
-        </ul>
-      
+    <nav className="w-full md:w-auto">
+      { }
+
+      <ul className="w-full sm:flex flex-col sm:flex-row items-center ">
+        {
+          navContent
+        }
+
+
+      </ul>
+
     </nav>
   );
 };
