855fb15496411b47b6e4962778c800c98498cc66
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-05-02 05:58:23 +0300

    fix: bug-fix in dashbaord

--- src/lib/nodemailer/email.ts
+++ src/lib/nodemailer/email.ts
@@ -10,6 +10,58 @@ const sendConfirmationEmail = async (
   Our representative will get back to you as soon as possible.\n\nBest regards,\n Wunderber Kenia Adventures.`
   ,amount:number=1200) => {
 let result:any={error:"",flag:false}
+const company = 'Wunderber Kenia Adventures'
+const html = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>{company} - Thank you for choosing us!</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        font-size: 16px;
+        color: #333333;
+        line-height: 1.5;
+      }
+      .container {
+        max-width: 600px;
+        margin: 0 auto;
+        padding: 20px;
+        border: 1px solid #cccccc;
+        border-radius: 5px;
+        background-color: #f8f8f8;
+      }
+      h1 {
+        font-size: 24px;
+        font-weight: bold;
+        margin-top: 0;
+        margin-bottom: 20px;
+      }
+      p {
+        margin-top: 0;
+        margin-bottom: 20px;
+      }
+      .signature {
+        font-weight: bold;
+        margin-top: 20px;
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <img src="https://dev2.wunderber.com/public/assets/logo.png" alt="{company} logo" width="150" height="50">
+      <h1>${company} - Thank you for contacting us!</h1>
+      <p>Hello ${name},</p>
+      <p>Thank you for contacting us through our website's contact form. We appreciate the opportunity to assist you and will do our best to provide you with the support you need.</p>
+      <p>One of our team members will review your message and get back to you as soon as possible. Please note that our business hours are {hours}, and we typically respond to inquiries within {response_time}.</p>
+      <p>If your request is urgent, please don't hesitate to give us a call at {phone} or chat with us live on our website. We are always here to help!</p>
+      <p>Thank you again for reaching out to us. We look forward to speaking with you soon.</p>
+      <div class="signature">Best regards,<br>{signature_name}<br>${company}</div>
+    </div>
+  </body>
+</html>
+`
 let info1;
   try {
     // Create a nodemailer transport object
@@ -33,7 +85,7 @@ let info1;
       to,
       // bcc:process.env.NEXT_PUBLIC_EMAIL_USER,
       subject,
-      text: body
+      html: html
     };
 
     // Send the email
