f3907f3700f13715d8cdcc3b6b1565d8aede944a
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-05-07 02:35:42 +0300

    fix : bug-fix in dashboard

--- src/app/dashboard/tours/page.tsx
+++ src/app/dashboard/tours/page.tsx
@@ -1,6 +1,5 @@
 import { API_END } from "@/constants/dashboard";
-import Link from "next/link";
-import TourItem from "./tourItem/page";
+import TourItem from "../../../components/layout/tourItem";
 
 async function getTours() {
   const res = await fetch(`${API_END}dashboard/tours`, { cache: "no-store" });
@@ -10,11 +9,7 @@ async function getTours() {
   return await res.json();
 }
 
-async function deleteTour(id: string) {
-  return await (
-    await fetch(`${API_END}dashboard/tours/${id}`, { cache: "no-store" })
-  ).json();
-}
+
 
 export interface IBookedTour {
   _id?: string;
@@ -28,40 +23,6 @@ export interface IBookedTour {
   phone: any;
   status: any;
 }
-// const TourItem = ({
-//   id,
-//   _id,
-//   created_at,
-//   createdAt,
-//   departureDate,
-//   country,
-//   name,
-//   email,
-//   phone,
-// }: IBookedTour) => (
-//   <div className="p-2 grid grid-cols-12 gap-1 border-1  border-red-400">
-//     <div className="col-span-1 overflow-hidden p-1 flex-2">{name}</div>
-//     <div className="col-span-1 overflow-hidden p-1 flex-2">{email}</div>
-//     <div className="col-span-1 overflow-hidden p-1 flex-1">{phone}</div>
-//     <div className="col-span-1 overflow-hidden p-1 flex-1">{country}</div>
-//     {/* <div className="col-span-1 overflow-hidden p-1 flex-1">{status}</div> */}
-//     <div className="col-span-1 overflow-hidden p-1 flex-2">{`${(
-//       (created_at as Date) || createdAt
-//     ).valueOf()}`}</div>
-//     <div className="col-span-1 overflow-hidden p-1 flex-1">{`${departureDate.valueOf()}`}</div>
-//     <div className="col-span-1 overflow-hidden p-1 flex-1">
-//       <span
-//         className="w-full rounded-md bg-red-400 p-2"
-//         onClick={() => deleteTour(id ? id : _id ? _id : "")}
-//       >
-//         x
-//       </span>
-//     </div>
-//     <div className="col-span-1 overflow-hidden p-1 flex-1">
-//       <Link href={`/dashboard/tours/${id || _id}`}> view</Link>
-//     </div>
-//   </div>
-// );
 
 const titiles = (
   <div className="p-2 grid grid-cols-12 gap-1 border-1 border-red-400 font-bold ">
