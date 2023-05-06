b99ed9ed856671fd75ed95c72561762e513b1ae9
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-05-07 01:01:22 +0300

    fix : bug-fix in dashboard

--- src/pages/api/dashboard/index.ts
+++ src/pages/api/dashboard/index.ts
@@ -1,7 +1,7 @@
 
 import mongoDB from "@/lib/mongoose";
 import { ContactMail, TourBooking, User } from "@/models/mongoose";
-import { ITourBooking } from "@/models/mongoose/TourBooking.model";
+// import { ITourBooking } from "@/models/mongoose/TourBooking.model";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -11,15 +11,14 @@ export default async function handler(
   if (req.method?.toLocaleLowerCase() === "get") {
     try {
       await mongoDB();
-const tourByCountry = await (await TourBooking.find()).map(({country, email,createdAt}:ITourBooking)=>{country, email,createdAt})
-      const [numTours, numUsers, numConactMails,toursCountry] = await Promise.all([
+      const [numTours, numUsers, numConactMails, toursCountry] = await Promise.all([
         TourBooking.count(),
         User.count(),
         ContactMail.count(),
-        tourByCountry
+        TourBooking.find({}, { email: 1, country: 1, createdAt: 1 })
       ]);
 
-      res.status(200).json({ numTours, numUsers, numConactMails, toursCountry });
+      res.status(200).json({ nums: { numTours, numUsers, numConactMails }, toursCountry });
       // res.send({data:"hello"})
     } catch (error: any) {
       console.log({ error: error.message });
