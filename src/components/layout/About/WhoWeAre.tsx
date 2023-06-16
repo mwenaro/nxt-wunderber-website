1d01d9020839084eccbe9bf943ff447f845f74e6
Mwero Abdalla <mwerothewebmaker@gmail.com>
2023-06-16 17:44:39 +0300

    chore: add content

--- src/components/layout/About/WhoWeAre.tsx
+++ src/components/layout/About/WhoWeAre.tsx
@@ -9,7 +9,7 @@ const WhoWeAre: React.FC = () => {
         Wunderber Kenya Tours and Safari is a premier travel management company that offers unforgettable holiday packages and wildlife safaris in East Africa, specifically in Kenya and Tanzania. We take pride in our extensive experience, unwavering dedication, unparalleled professionalism, steadfast commitment, and outstanding reliability that have earned us a reputation as the most preferred and reliable tour operator in the region.
       </p>
       <p className="text-lg mb-4">
-        Our expertise and knowledge of the wildlife history and habitat, coupled with our ability to provide safari holiday planning and ground handling services to online and overseas travel agents, have made us one of the most sought-after safari and tour operators in East Africa.
+        Our expertise and knowledge of the wildlife history and habitat, coupled  with our ability to provide safari holiday planning and ground handling services to online and overseas travel agents, have made us one of the most sought-after safari and tour operators in East Africa.
       </p>
       <p className="text-lg mb-4">
         At Wunderber Kenya Tours and Safari, we understand that a safari, holiday, or honeymoon in Africa is often a lifetime dream and a significant investment. Thus, we make it our mission to provide our guests with genuine and expert advice that is tailored to their interests, budget, and season of travel. We take the time to select the right destinations, lodges, camps, and hotels that fit our guests&apos; itinerary specifications to ensure that they receive a one-of-a-kind experience that meets their expectations.
